dad4f342093f8c00190566adf89601b1ab22ea4a
Kseniya Antipina <karinpark@mail.ru>
2021-10-26 21:03:18 +0300

    Updates

---
+++ precache-manifest.ec11a1eeb4d19883771687ada6175e13.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d49a4ea9853fd65888278b67ead438",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "9bb5c882e3ba08948c7f",
+    "url": "/memory-game/static/css/main.a6099375.chunk.css"
+  },
+  {
+    "revision": "cece681ab1d4a02ccfd3",
+    "url": "/memory-game/static/js/2.6668c4c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.6668c4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb5c882e3ba08948c7f",
+    "url": "/memory-game/static/js/main.d0f20280.chunk.js"
+  },
+  {
+    "revision": "7a16f6cba289a3b0f38e",
+    "url": "/memory-game/static/js/runtime-main.67e2c1a5.js"
+  },
+  {
+    "revision": "4d4671a28c1fca42d94ced9b40ccddfc",
+    "url": "/memory-game/static/media/back.4d4671a2.jpg"
+  },
+  {
+    "revision": "3587a4ae70da87789558841767106261",
+    "url": "/memory-game/static/media/kakashi.3587a4ae.jpg"
+  },
+  {
+    "revision": "b40ba52296f77602025e6d1c30b97f97",
+    "url": "/memory-game/static/media/kushina.b40ba522.png"
+  },
+  {
+    "revision": "d6c595a8ca58cb51c904b1c945911a19",
+    "url": "/memory-game/static/media/naruto.d6c595a8.jpg"
+  },
+  {
+    "revision": "597e7d5a0665a6636057e1fb3139c7e6",
+    "url": "/memory-game/static/media/neji.597e7d5a.jpg"
+  },
+  {
+    "revision": "c756f22963ca25816718a8d614267b3b",
+    "url": "/memory-game/static/media/rockli.c756f229.jpg"
+  },
+  {
+    "revision": "6a154d9c64076ef7828c56bd832abb32",
+    "url": "/memory-game/static/media/sakura.6a154d9c.jpg"
+  },
+  {
+    "revision": "7ef68d7bb0dc867e0108d396137b1620",
+    "url": "/memory-game/static/media/saske.7ef68d7b.jpg"
+  },
+  {
+    "revision": "1a6d123716ec912de44e99b750df0d53",
+    "url": "/memory-game/static/media/shikamaru.1a6d1237.jpg"
+  }
+]);
